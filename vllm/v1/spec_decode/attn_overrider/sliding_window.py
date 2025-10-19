b5e0bfb9482d97f9a16d270b5d9c52cd99d37b0d
Yikang Yue <yikangy2@illinois.edu>
2025-10-18 22:09:52 -0500

    Fix bug in sliding window

--- vllm/v1/spec_decode/attn_overrider/sliding_window.py
+++ vllm/v1/spec_decode/attn_overrider/sliding_window.py
@@ -24,10 +24,21 @@ class SlidingWindowAttentionOverrider(AbstractAttentionOverrider):
         self.window_size = window_size
         self.window_num_blocks = window_size // self.block_size
         self.max_block_slot = self.max_model_len // self.block_size - 1
+        self.num_speculative_tokens = \
+            vllm_config.speculative_config.num_speculative_tokens
         self.offset = torch.arange(
             -self.window_num_blocks + 1, 1, 1,
             dtype=torch.int32, device=device
         )
+
+        # Whether to enable sliding window attention.
+        self._enable_sliding_window = False
+
+        # Lazy allocation of self-maintained block_table
+        self.block_table = None
+
+    def ready_to_draft(self):
+        return self._enable_sliding_window
 
     def _overriden_kv_insert(self, *args, **kwargs):
         # Always use the original kv insert function.
@@ -35,8 +46,15 @@ class SlidingWindowAttentionOverrider(AbstractAttentionOverrider):
 
     def _overriden_attention(self, *args, **kwargs):
         # If not in draft phase, use the original attention function.
+        # Besides, determine whether to enable sliding window attention.
         if not self.in_draft:
+            max_seqlen_q = kwargs['max_seqlen_q']
+            self._enable_sliding_window = \
+                max_seqlen_q <= self.num_speculative_tokens + 1
             return self.original_attention_func(*args, **kwargs)
+
+        # If not enabling sliding window, cannot reach here.
+        assert self._enable_sliding_window
 
         # Since all attention layers share the same attn_metadata,
         # we only need to modify it for the first layer of the model.
@@ -47,6 +65,10 @@ class SlidingWindowAttentionOverrider(AbstractAttentionOverrider):
             # Get the necessary arguments.
             seq_lens: torch.Tensor = kwargs['seqused_k']
             block_table: torch.Tensor = kwargs['block_table']
+            batch_size = seq_lens.shape[0]
+            if self.block_table is None or \
+                    self.block_table.shape[0] < batch_size:
+                self.block_table = torch.zeros_like(block_table)
 
             # Compute the sliding window attention blocks.
             tail_slots = (seq_lens // self.block_size).unsqueeze_(-1)
@@ -54,8 +76,9 @@ class SlidingWindowAttentionOverrider(AbstractAttentionOverrider):
             window_slots = tail_slots.expand(-1, self.window_num_blocks)
             window_slots = window_slots + self.offset
             # Keep the first block as the attention sink.
-            # block_table is a tensor, only needs to modify once.
-            block_table[:, 1: self.window_num_blocks + 1] = \
+            self.block_table[:batch_size, 0] = block_table[:batch_size, 0]
+            # Copy the sliding window blocks.
+            self.block_table[:batch_size, 1: self.window_num_blocks + 1] = \
                 torch.gather(block_table, 1, window_slots)
 
             # Compute the seq_lens for the sliding window attention.
@@ -64,12 +87,27 @@ class SlidingWindowAttentionOverrider(AbstractAttentionOverrider):
                 extra_lens + self.block_size - 1,
                 self.block_size, rounding_mode='floor'
             ).clamp_min_(0)
-            # seq_lens is a tensor, only needs to modify once.
-            seq_lens -= extra_blocks * self.block_size
-            self.max_seq_len = seq_lens.max().item()
 
-        # max_seq_len is an integer, needs to modify every time.
-        kwargs['max_seqlen_k'] = self.max_seq_len
+            # Store the modified seq_lens and max_seq_len.
+            self.seqused_k = seq_lens - extra_blocks * self.block_size
+            self.max_seqlen_k = self.seqused_k.max().item()
+
+        # Store the original attn_metadata.
+        original_block_table = kwargs['block_table']
+        original_seqused_k = kwargs['seqused_k']
+        original_max_seqlen_k = kwargs['max_seqlen_k']
+
+        # # Update kwargs with modified attn_metadata.
+        kwargs['block_table'] = self.block_table
+        kwargs['seqused_k'] = self.seqused_k
+        kwargs['max_seqlen_k'] = self.max_seqlen_k
 
         # Call the original attention function with modified attn_metadata.
-        return self.original_attention_func(*args, **kwargs)
+        rst = self.original_attention_func(*args, **kwargs)
+
+        # Restore the original attn_metadata in kwargs.
+        kwargs['block_table'] = original_block_table
+        kwargs['seqused_k'] = original_seqused_k
+        kwargs['max_seqlen_k'] = original_max_seqlen_k
+
+        return rst
